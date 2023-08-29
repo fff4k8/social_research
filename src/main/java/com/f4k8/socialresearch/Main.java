c85f620d114aea9d4aeb9c74672610e27f7025b2
fff4k8 <4k8@mail.ru>
2023-08-29 20:29:43 +0400

    initial commit

--- src/main/java/com/f4k8/socialresearch/Main.java
+++ src/main/java/com/f4k8/socialresearch/Main.java
@@ -1,8 +1,13 @@
 package com.f4k8.socialresearch;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(Main.class, args);
     }
 }
